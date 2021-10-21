4545051ad3a39de4f3c9aea9cfe5caa2b819ba3d
rcmgn <rcmgn@yandex.ru>
2021-10-21 15:20:22 +0500

    First release

--- wordcount.go
+++ wordcount.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Check workflow")
+	cnt, err := readInput()
+	if err != nil {
+		fail(err)
+	}
+	fmt.Println(cnt)
+}
+
+// readInput reads pattern and source string
+// from command line arguments and returns them.
+func readInput() (res int, err error) {
+	flag.Parse()
+	// fmt.Println(flag.Args())
+	str := strings.Join(flag.Args(), " ")
+	// fmt.Println(str)
+	res = len(strings.Split(str, " "))
+	return res, nil
+}
+
+// fail prints the error and exits.
+func fail(err error) {
+	fmt.Println("wordcount:", err)
+	os.Exit(1)
 }
