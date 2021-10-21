7b53c8a9c344ed5bb2526e9597955dbbad7d784a
rcmgn <rcmgn@yandex.ru>
2021-10-21 15:26:36 +0500

    remove exit on error

--- wordcount.go
+++ wordcount.go
@@ -3,31 +3,21 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
 	"strings"
 )
 
 func main() {
-	cnt, err := readInput()
-	if err != nil {
-		fail(err)
-	}
+	cnt := readInput()
 	fmt.Println(cnt)
 }
 
 // readInput reads pattern and source string
 // from command line arguments and returns them.
-func readInput() (res int, err error) {
+func readInput() (res int) {
 	flag.Parse()
 	// fmt.Println(flag.Args())
 	str := strings.Join(flag.Args(), " ")
 	// fmt.Println(str)
 	res = len(strings.Split(str, " "))
-	return res, nil
-}
-
-// fail prints the error and exits.
-func fail(err error) {
-	fmt.Println("wordcount:", err)
-	os.Exit(1)
+	return res
 }
